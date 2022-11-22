767ebc85a17df15ca82cb85ba427a6f4262d7bfe
Petr Tcoi <petrtcoi@outlook.com>
2022-11-23 01:46:15 +0300

    шаблон редактирования

--- src/components/DataTable/DataTable.types.ts
+++ src/components/DataTable/DataTable.types.ts
@@ -1,16 +1,13 @@
 import { WorkGetDto } from '../../typescript/work.type'
 
 
-
 export type WorkWithMeta = WorkGetDto & { _meta_: WorkMeta }
-
 
 export type WorkMeta = {
   level: WorkLevel
   hasChild: boolean
   isLastChild?: boolean
 }
-
 export type WorkLevel = 1 | 2 | 3
 export type SomeWorkLevel = WorkLevel | -1
 
